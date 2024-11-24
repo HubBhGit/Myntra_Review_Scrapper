e42a3c28c78d2b42e5e8bc50b8be2abf7eb98c8f
Bharath.R <bharathr27097@gmail.com>
2024-11-24 12:18:03 +0530

    File Update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="scrapper",
+    version="0.0.1",
+    description="It's a ML project",
+    author="user",
+    author_email="user@example.com",
+    packages=find_packages(),
+    install_requires=[]
+)
